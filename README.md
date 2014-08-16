f901e9c137d7f1b9931614cf369251d7d38d2d35
Niraj Pant <nirajpant7@gmail.com>
2014-08-16 01:02:53 -0500

    Added bubble example

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Data-Structures-and-Algorithms
 ==============================
 
 Data Structures and Algorithms in Java.
+
+## Approach
+I have tried to make each example as clear and possible to understand the concepts.
---
+++ Sorts/bubble.java
@@ -0,0 +1,44 @@
+package bubblesort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        
+        int temp = 0;
+        boolean flag = true;
+        
+        //populate array
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = ((int)(Math.random() * 20));
+            System.out.print("[" + nums[i] + "]");           
+        }
+        
+        System.out.println("");
+            
+        //Apply Bubble Sort
+        while(flag) {
+            flag = false;
+            for (int i = 0; i < (nums.length-1); i++) {
+                if (nums[i+1] < nums[i]) {
+                    temp = nums[i+1];
+                    nums[i+1] = nums[i];
+                    nums[i] = temp;
+                    flag = true;
+                }            
+            }   
+        } 
+        
+        //print results
+        System.out.println("");
+                
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("[" + nums[i] + "]");
+        }
+        
+        
+        //bubble sort
+        
+    }
+    
+}

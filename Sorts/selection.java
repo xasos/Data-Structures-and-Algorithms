b7ee0f81d863a7b2a9e23e7135a517ed211a792c
Niraj Pant <nirajpant7@gmail.com>
2014-08-17 13:41:41 -0500

    added selection

---
+++ Sorts/selection.java
@@ -0,0 +1,35 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int temp;
+        int first;
+        
+        // populate array
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = ((int)(Math.random() * 20));
+            System.out.print("[" + nums[i] + "]");           
+        }
+        
+        // apply selection sort
+        for (int i = 0; i < nums.length; i++) {
+            first = i; // change starting point every time loop is entered
+            for (int j = i; j < nums.length; j++) {
+                if (nums[first] > nums[j]) {
+                    first = j;
+                }
+            }     
+            
+            temp = nums[i];
+            nums[i] = nums[first];
+            nums[first] = temp;            
+        }
+        
+        //print out sorted array
+        System.out.println("");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print("[" + nums[i] + "]");           
+        }             
+    }
+    
+}
